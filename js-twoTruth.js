f1f96c3748e4782da4818c78649b3356bb7681df
Benjamin Scott Andersen <64924716+benjaminandersen01@users.noreply.github.com>
2021-02-11 20:48:05 -0600

    Add files via upload

---
+++ js-twoTruth.js
@@ -0,0 +1,10 @@
+function truth1Function() {
+    document.getElementById("truth1").style.color = 'green';
+    console.log("fndfjskalnfjkdlsn");
+}
+function truth2Function() {
+    document.getElementById("truth2").style.color = 'green';
+}
+function lie1Function() {
+    document.getElementById("lie1").style.color = 'grey';
+}
